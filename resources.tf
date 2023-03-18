e9cae1cf51bb3ab91d38ea3507250d49aca5475e
Adeyemi Adeleye <countyemi@gmail.com>
2023-03-18 13:25:03 -0400

    dynamo table lock created

--- resources.tf
+++ resources.tf
@@ -26,3 +26,16 @@ resource "aws_s3_bucket" "state-bucket" {
       }
     }
 }
+
+#create the dynamo table lock to be used by state-bucket
+resource "aws_dynamodb_table" "terraform_locks" {
+name         = "state-bucket-locks"
+hash_key     = "LockID"
+read_capacity = 3
+write_capacity = 3
+
+attribute {
+name = "LockID"
+type = "S"
+      }
+}
